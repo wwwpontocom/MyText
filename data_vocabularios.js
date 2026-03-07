439b8d4a3594f5812f23eb62ec340fcfb4b67cd1
wwwpontocom <wwwpontoscom@gmail.com>
2026-03-07 00:16:42 -0300

    Create data_vocabularios.js

---
+++ data_vocabularios.js
@@ -0,0 +1,10 @@
+const ptVocabularyA = [
+    "abaixo", "acima", "aberto", "abrir", "abacate", "abacaxi", "abafar", 
+    "abaixar", "abalar", "abanar", "abandonar", "abençoar", "abertura", 
+    "abismo", "abóbora", "abotoar", "abraçar", "abraço", "abranger", 
+    "abrigar", "abrigo", "abril", "absoluto", "absurdo", "abundância", 
+    "acabar", "academia", "acalmar", "acampamento", "ação", "acaso", 
+    "aceitar", "acenar", "acento", "acerto", "aceso", "achar", "acidente", 
+    "ácido", "acionar", "aclarar", "acolher", "acomodar", "acompanhar", 
+    "aconselhar", "acontecer", "acordar", "acordo", "acreditar", "açúcar"
+];
