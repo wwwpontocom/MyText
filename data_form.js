c4475370629300245e027401210d21c203ceda3f
wwwpontocom <wwwpontoscom@gmail.com>
2026-03-08 22:25:23 -0300

    Update data_form.js

--- data_form.js
+++ data_form.js
@@ -1,7 +1,7 @@
 // 1. Initial Library Definition
 let BIBLIOTECA_LIVRO = {
     "form_space_order": {
-        keywords: ["arquitetura", "forma", "espaço", "ordem", "ching", "proporção", "ritmo", "hierarquia", "simetria", "Ponto Indica uma posição no espaço Linha Um ponto estendido possui comprimento direção e posição Plano Uma linha estendida possui largura e comprimento Volume Um plano estendido possui profundidade"],
+        keywords: ["arquitetura", "forma", "espaço", "ordem", "ching", "proporção", "ritmo", "hierarquia", "simetria", "ponto", "linha", "comprimento", "direção", "posição", "plano", "largura", "comprimento", "volume", "profundidade"],
         fase: "TEORIA DA ARQUITETURA",
         titulo: "Forma, Espaço e Ordem",
         icone: "🏛️",
